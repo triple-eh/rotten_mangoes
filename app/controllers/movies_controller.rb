6aeed766004a78829e553b08fba65a65a82d3d12
triple-eh <aleckaleksandrov@gmail.com>
2016-06-20 16:55:23 -0700

    implemented partials

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,40 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movie_path
+  end
+
+  protected
+    def movie_params
+      params.require(:movie).permit(
+        :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+      )
+    end
+end
