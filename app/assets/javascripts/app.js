ecea528a55255217a35b593ab98b45557c0c7a8b
triple-eh <aleckaleksandrov@gmail.com>
2016-06-24 15:18:08 -0700

    Applied materialize

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,9 @@
-$(document).ready( function() {
-   $(".button-collapse").sideNav();
-})
+// $(document).ready( function() {
+//    $(".button-collapse").sideNav();
+//    $('select').material_select();
+// })  
+
+// window.onload = function() {
+//   $(".button-collapse").sideNav();
+//   $('select').material_select();
+// }
